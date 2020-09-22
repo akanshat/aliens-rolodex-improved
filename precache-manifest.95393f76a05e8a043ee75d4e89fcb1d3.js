09b80bbcbfde4c4d80043eb6cced7494a1be908d
akanshat <akanshat1999@gmail.com>
2020-09-22 13:48:45 +0530

    Updates

---
+++ precache-manifest.95393f76a05e8a043ee75d4e89fcb1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3e4148f5326db6ea948e316d2d0bf3",
+    "url": "/aliens-rolodex-improved/index.html"
+  },
+  {
+    "revision": "5406c7dcb814c4c8d6e6",
+    "url": "/aliens-rolodex-improved/static/css/main.0cf27a2c.chunk.css"
+  },
+  {
+    "revision": "5062a1066a105645471a",
+    "url": "/aliens-rolodex-improved/static/js/2.f62f64bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aliens-rolodex-improved/static/js/2.f62f64bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5406c7dcb814c4c8d6e6",
+    "url": "/aliens-rolodex-improved/static/js/main.778b94f4.chunk.js"
+  },
+  {
+    "revision": "c119f73b10148095b199",
+    "url": "/aliens-rolodex-improved/static/js/runtime-main.12ec8c60.js"
+  }
+]);
